e30b17edeb190e2de9aef852c80f5df1d64e7446
JonJon Timothy Llavora <j.llavora@damstratechnology.com>
2020-12-14 14:58:18 +0800

    working sort

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const useStyles = createUseStyles({
         width: '100%',
         padding: '7rem 0',
     },
-    
+
     endText: {
         textAlign: 'center',
         fontSize: '1.6rem',
@@ -43,7 +43,7 @@ const App = () => {
     const [nextListItems, setNextListItems] = useState([]);
     const [nextPage, setNextPage] = useState(1);
     const [sortString, setSortString] = useState('title');
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [isSortLoading, setIsSortLoading] = useState(false);
     const [isShownAll, setIsShownAll] = useState(false);
     const [isDataFinished, setIsDataFinished] = useState(false);
@@ -72,15 +72,18 @@ const App = () => {
     });
 
     const getAsciiFacesRequest = (initialLoad) => {
+        setIsLoading(true);
         asciiFaceRequest.call().then((data) => {
             if (data.results.length === 0) {
                 setIsDataFinished(true);
             } else {
-                setNextPage(nextPage + 1);
+                setNextPage(data.nextPage);
 
                 initialLoad
                     ? setListItems([...data.results])
                     : setNextListItems([...data.results]);
+
+                setIsLoading(false);
             }
         });
     };
@@ -113,10 +116,21 @@ const App = () => {
         }
     };
 
+    
+    const handleSort = (e) => {
+        setSortString(e.target.value);
+        setListItems([]);
+        setNextListItems([]);
+        setNextPage(1);
+        setIsShownAll();
+        setIsDataFinished();
+    }
+
     const fetchData = async () => {
         await getAsciiFacesRequest(true);
         await getAsciiFacesRequest(false);
     };
+
 
     useEffect(() => {
         fetchData();
@@ -130,17 +144,10 @@ const App = () => {
         };
     });
 
-    const handleSort = (e) => {
-        setIsSortLoading(true);
-        setIsLoading(false);
-
-        setTimeout(() => {
-            const sortedList = listItems.sort(getSortOrder(e.target.value));
-            setListItems(sortedList);
-
-            setIsSortLoading(false);
-        }, 1000);
-    };
+    useEffect(() => {
+        getAsciiFacesRequest(true);
+        getAsciiFacesRequest(false);
+    }, [sortString]);
 
     const renderItems = () => {
         var asciiFacesOutput = [];
@@ -192,19 +199,22 @@ const App = () => {
         <div className={classes.mainContainer}>
             <Grid variety={GRID_ROW}>
                 <Grid variety={COL_2_OF_4}>
-                    <Select options={sortOptions} onChange={handleSort} />
+                    <Select
+                        options={sortOptions}
+                        onChange={handleSort}
+                    />
                 </Grid>
             </Grid>
 
             <Grid variety={GRID_ROW}>
                 <Fragment>
-                    {isSortLoading ? <Spinner /> : renderItems()}
+                    {renderItems()}
                 </Fragment>
             </Grid>
 
             <Grid variety={GRID_ROW}>
                 {isLoading && !isShownAll && <Spinner />}
-                {isShownAll && !isSortLoading && (
+                {isShownAll && !isLoading && (
                     <p className={classes.endText}>~ end of catalogue ~</p>
                 )}
             </Grid>
