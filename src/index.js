9015c43529ebea1f9ba4657d065c83d94db5b8cd
Jon-Jon Timothy Llavora <kkbjjtllavora@gmail.com>
2020-12-14 16:55:16 +0800

    remove js-time

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
-
-TimeAgo.addDefaultLocale(en);
 
 ReactDOM.render(
     <React.StrictMode>
